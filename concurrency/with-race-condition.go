205660b7463966b822c34c787efde0d2b11950c3
Illia Kripaka <kripakaillya@gmail.com>
2020-07-02 12:55:55 +0300

    Delete test files/ Add "timer" in two files/ Add atomic func

--- concurrency/with-race-condition.go
+++ concurrency/with-race-condition.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	//"math/rand"
 	//"time"
 
 	//"time"
 )
 
-
 func main() {
 	var counter int
 	channel := make(chan int)
+	timeBefore := time.Now()
+	counter++
+	fmt.Println(counter)
 	go increment("<1>", channel, &counter)
 	go increment("<2>", channel, &counter)
 	if (<-channel + <-channel) == 2 {
-		fmt.Println("Counter: ", counter)
+		fmt.Println("Counter: ", counter, "Time:", time.Now().Sub(timeBefore))
 		close(channel)
 	}
 }
@@ -23,8 +27,7 @@ func main() {
 func increment(processName string, channel chan int, counter *int) {
 	for i := 0; i < 1000000; i++ {
 		*counter++
-		fmt.Println(processName, "|Iteration:", i, "|Counter:", &counter)
+		//fmt.Println(processName, "|Iteration:", i, "|Counter:", *counter)
 	}
-	fmt.Print("")
 	channel <- 1
 }
--- concurrency/without-race-condition.go
+++ concurrency/without-race-condition.go
@@ -3,27 +3,41 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 var counterMutex sync.Mutex
 
 func main() {
-	var counter int
+	var counter int32
 	channel := make(chan int)
+	timeBefore := time.Now()
 	go incrementWithMutex("<1>", channel, &counter)
 	go incrementWithMutex("<2>", channel, &counter)
+//	go incrementWithAtomic("<2>", channel, &counter)
+//	go incrementWithAtomic("<2>", channel, &counter)
+
 	if (<-channel + <-channel) == 2 {
-		fmt.Println("Counter: ", counter)
+		fmt.Println("Counter: ", counter, "Time:", time.Now().Sub(timeBefore))
 		close(channel)
 	}
 }
 
-func incrementWithMutex(processName string, channel chan int, counter *int) {
+func incrementWithMutex(processName string, channel chan int, counter *int32) {
 	for i := 0; i < 1000000; i++ {
 		counterMutex.Lock()
 		*counter++
 		counterMutex.Unlock()
-		fmt.Println(processName, "|Iteration:", i, "|Counter:", &counter)
+		//fmt.Println(processName, "|Iteration:", i, "|Counter:", &counter)
+	}
+	channel <- 1
+}
+
+func incrementWithAtomic(processName string, channel chan int, counter *int32) {
+	for i := 0; i < 1000000; i++ {
+		atomic.AddInt32(counter, 1)
+		//fmt.Println(processName, "|Iteration:", i, "|Counter:", &counter)
 	}
 	channel <- 1
 }
