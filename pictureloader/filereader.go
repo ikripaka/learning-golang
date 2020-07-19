1d3943b020c367677a8d8080c8f4bf25878d9ac7
Illia Kripaka <kripakaillya@gmail.com>
2020-07-19 21:58:15 +0300

    Make reading simpler

--- pictureloader/filereader.go
+++ pictureloader/filereader.go
@@ -1,70 +1,23 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
-	"os"
+	"strings"
 	"sync"
 )
 
 // Reads file with urls and pushes them to the buffered channel
 // filePath - path to file that contain urls
 func ReadPictureUrls(filePath string, chanWithUrls chan string, waitGroup *sync.WaitGroup) {
-
-	file, err := os.Open(filePath)
+	allFileInByte, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Problems with reading file")
 	}
-	defer func() {
-		if err = file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		chanWithUrls <- scanner.Text()
-		err = scanner.Err()
-		if err != nil {
-			log.Fatal(err)
-		}
+	urlsSlice := strings.Split(string(allFileInByte), "\n")
+	for _, val := range urlsSlice {
+		chanWithUrls <- val
 	}
 	close(chanWithUrls)
 	waitGroup.Done()
-	fmt.Println("done read -------")
-
-}
-
-// Counts file lines for creating buffered channel
-// filePath - path to file to count lines in file
-func countLines(filePath string) (int, error) {
-	reader, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var count int
-	const lineBreak = '\n'
-
-	buffer := make([]byte, bufio.MaxScanTokenSize)
-	for {
-		bufferSize, err := reader.Read(buffer)
-		if err != nil && err != io.EOF {
-			return 0, err
-		}
-		var bufferPosition int
-		for {
-			i := bytes.IndexByte(buffer[bufferPosition:], lineBreak)
-			if i == -1 || bufferSize == bufferPosition {
-				break
-			}
-			bufferPosition += i + 1
-			count++
-		}
-		if err == io.EOF {
-			break
-		}
-	}
-	return count + 1, nil
 }
